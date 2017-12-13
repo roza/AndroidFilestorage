c149933aae18a8d479ce79438e2f27e3197c0817
Gérard Rozsavolgyi <roza@univ-orleans.fr>
2017-12-13 12:09:28 +0100

    added writer and reader to json

--- app/src/main/java/com/example/android/roza/androidfilestorage/MainActivity.java
+++ app/src/main/java/com/example/android/roza/androidfilestorage/MainActivity.java
@@ -4,25 +4,34 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.util.JsonWriter;
+import android.util.JsonReader;
+
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Date;
 
 public class MainActivity extends Activity {
 
     EditText textmsg;
+    TextView jsonmsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textmsg = findViewById(R.id.editText1);
+        jsonmsg = findViewById(R.id.textView2);
     }
 
     /** Ecrire le texte vers le fichier */
-    public void writeBtn(View v) {
+    public void writeText(View v) {
         try {
             FileOutputStream fileout=openFileOutput("monfichier.txt", MODE_PRIVATE);
             OutputStreamWriter writer=new OutputStreamWriter(fileout);
@@ -38,11 +47,12 @@ public class MainActivity extends Activity {
     }
 
     /** Effacer l'EditText */
-    public void clearBtn(View v) {
+    public void clearEdit(View v) {
         textmsg.setText("");
     }
+
     /** Lire le texte à partir du fichier */
-    public void readBtn(View v) {
+    public void readText(View v) {
         try {
             FileInputStream fis = openFileInput("monfichier.txt");
             InputStreamReader reader= new InputStreamReader(fis);
@@ -61,5 +71,59 @@ public class MainActivity extends Activity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+    public void writeJson(View view) throws IOException {
+        FileOutputStream fileout=openFileOutput("monfichier.json", MODE_PRIVATE);
+        JsonWriter writer = new JsonWriter(new OutputStreamWriter(fileout, "UTF-8"));
+        writer.setIndent("  ");
+        writer.beginObject();
+        writer.name("date").value(new Date().toString());
+        writer.name("text").value(textmsg.getText().toString());
+        writer.endObject();
+        writer.close();
+    }
+
+    public void readJson(View v) {
+        try {
+            FileInputStream fis = openFileInput("monfichier.json");
+            InputStreamReader reader= new InputStreamReader(fis);
+
+            int charRead = fis.available(); // taille du fichier
+            char[] inputBuffer= new char[charRead];
+            final int read = reader.read(inputBuffer, 0, charRead);
+            if (read>0) {
+                jsonmsg.setText(new String(inputBuffer));
+            } else {
+                jsonmsg.setText(R.string.Read_Warn);
+            }
+
+            reader.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void readJsonObject(View view) throws IOException {
+     String date = null;
+     String text = null;
+     FileInputStream fis = openFileInput("monfichier.json");
+     JsonReader reader= new JsonReader(new InputStreamReader(fis, "UTF-8"));
+     reader.beginObject();
+     while (reader.hasNext()) {
+       String name = reader.nextName();
+       if (name.equals("date")) {
+         date = reader.nextString();
+       } else if (name.equals("text")) {
+         text = reader.nextString();
+       }
+       else {
+         reader.skipValue();
+       }
+     }
+     reader.endObject();
+     reader.close();
+     jsonmsg.setText("date:"+date+", text:"+text);
     }
 }
